701614614a2d749fccc4a2642405375dfcc92852
Paulo Freitas <42820569+paulofreitas-py@users.noreply.github.com>
2023-10-11 16:21:11 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -9,3 +9,25 @@ ScrollReveal().reveal('.post', { delay: 100 });
 ScrollReveal().reveal('.post-data-like', { delay: 200 });
 ScrollReveal().reveal('.title-post', { delay: 300 });
 ScrollReveal().reveal('.post-summary', { delay: 400 });
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
+});
+
+ScrollReveal().reveal('.post', { delay: 100 });
+ScrollReveal().reveal('.post-data-like', { delay: 200 });
+ScrollReveal().reveal('.title-post', { delay: 300 });
+ScrollReveal().reveal('.post-summary', { delay: 400 });
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
+});
+
+ScrollReveal().reveal('.post', { delay: 100 });
+ScrollReveal().reveal('.post-data-like', { delay: 200 });
+ScrollReveal().reveal('.title-post', { delay: 300 });
+ScrollReveal().reveal('.post-summary', { delay: 400 });
